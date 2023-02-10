b0a3e7d3efe81736ea181086881c7166ee0c6e75
Shubham-Hatkar <117711355+Shubham-Hatkar@users.noreply.github.com>
2023-02-10 13:42:40 +0530

    Added solution - LeetHub

---
+++ Count distinct pairs with difference k - GFG/count-distinct-pairs-with-difference-k.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s1 = br.readLine().trim();
+            String[] S1 = s1.split(" ");
+            String s2 = br.readLine().trim();
+            String[] S2 = s2.split(" ");
+            int n, k;
+            n = Integer.parseInt(S1[0]);
+            k = Integer.parseInt(S1[1]);
+            int[] nums = new int[n];
+            for(int i = 0; i < S2.length; i++)
+                nums[i] = Integer.parseInt(S2[i]);
+            Solution ob = new Solution();
+            int ans = ob.TotalPairs(nums, k);
+            System.out.println(ans);           
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int TotalPairs(int[] nums, int k)
+    {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+       
+        // add all ele in hm
+        for(int i = 0; i < nums.length; i++) 
+        hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        
+        int count = 0;
+        if(k == 0)
+        {
+            for(int key : hm.keySet())
+            {
+                if(hm.get(key) > 1) count++;
+            }
+        }
+        else
+        {
+            for(int key : hm.keySet())
+            {
+                if(hm.containsKey(k + key)) count++;
+            }
+        }
+        return count;
+    }
+}
