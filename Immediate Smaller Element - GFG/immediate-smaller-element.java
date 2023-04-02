0af1d5b38d71dda9c178d241e9162580ecccb3da
Shubham-Hatkar <117711355+Shubham-Hatkar@users.noreply.github.com>
2023-04-02 23:18:32 +0530

    Added solution - LeetHub

--- Immediate Smaller Element - GFG/immediate-smaller-element.java
+++ Immediate Smaller Element - GFG/immediate-smaller-element.java
@@ -39,10 +39,10 @@ class Solution
 {
     void immediateSmaller(int arr[], int n) 
     {
-        for(int i = 0; i < n-1; i++)
+        for(int i = 1; i < n; i++)
         {
-            if(arr[i] > arr[i+1]) arr[i] = arr[i+1];
-            else arr[i] = -1;
+            if(arr[i] < arr[i-1]) arr[i-1] = arr[i];
+            else arr[i-1] = -1;
         }
         arr[n-1] = -1;
     }
